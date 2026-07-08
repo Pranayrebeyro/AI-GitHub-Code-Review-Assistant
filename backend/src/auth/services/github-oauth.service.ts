e44deac2b953943775d5b996102e7813893f4b07
Pranayrebeyro <pranayrebeyro@gmail.com>
2026-07-08 20:51:09 +0530

    Debug OAuth config

--- backend/src/auth/services/github-oauth.service.ts
+++ backend/src/auth/services/github-oauth.service.ts
@@ -2,6 +2,9 @@ import qs from "qs";
 import { env } from "../../config/env.js";
 
 export function getGitHubOAuthURL() {
+  console.log("CLIENT_ID =", env.GITHUB_CLIENT_ID);
+  console.log("BACKEND_URL =", env.BACKEND_URL);
+
   const root = "https://github.com/login/oauth/authorize";
 
   const query = qs.stringify({
