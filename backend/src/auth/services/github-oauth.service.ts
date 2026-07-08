6013c0e9dc74c1966a2d69ecd3924cf8be5b4196
Pranayrebeyro <pranayrebeyro@gmail.com>
2026-07-08 19:49:24 +0530

    Remove OAuth debug code

--- backend/src/auth/services/github-oauth.service.ts
+++ backend/src/auth/services/github-oauth.service.ts
@@ -2,17 +2,11 @@ import qs from "qs";
 import { env } from "../../config/env.js";
 
 export function getGitHubOAuthURL() {
-  const redirectUri = `${env.BACKEND_URL}/api/auth/github/callback`;
-
-  throw new Error(
-    `BACKEND_URL=${env.BACKEND_URL}\nREDIRECT_URI=${redirectUri}`
-  );
-
   const root = "https://github.com/login/oauth/authorize";
 
   const query = qs.stringify({
     client_id: env.GITHUB_CLIENT_ID,
-    redirect_uri: redirectUri,
+    redirect_uri: `${env.BACKEND_URL}/api/auth/github/callback`,
     scope: "read:user user:email repo",
   });
 
